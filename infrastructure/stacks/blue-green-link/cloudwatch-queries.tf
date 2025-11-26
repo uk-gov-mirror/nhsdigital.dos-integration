513efc8bf65f6d585d597cfbea735ba98aadc6f1
ajmu1 <ajay.mudgal1@nhs.net>
2025-11-26 17:12:00 +0000

    Release/30.2 (#1140)
    # Release Branch Pull Request
    
    ## Description of Changes
    
    This release will contain upgrade of terraform core version(from v1.2.1
    to v1.13.0), terraform AWS provider version(v5.68.0 to v6.14.1) and
    python version upgrade(v3.12 to v3.14) for lambda and tester docker
    images of DI application as well.

--- infrastructure/stacks/blue-green-link/cloudwatch-queries.tf
+++ infrastructure/stacks/blue-green-link/cloudwatch-queries.tf
@@ -3,12 +3,12 @@
 // to propagate. Note: This does NOT guarantee post-create consistency
 // of the query definitions themselves, but can reduce immediate read
 // failures when the provider plans many resources concurrently.
-resource "time_sleep" "wait_for_propagation" {
-  create_duration = "5s"
-}
+# resource "time_sleep" "wait_for_propagation" {
+#   create_duration = "45s"
+# }
 
 resource "aws_cloudwatch_query_definition" "search_for_errors" {
-  depends_on = [time_sleep.wait_for_propagation]
+  # depends_on = [time_sleep.wait_for_propagation]
   name = "${var.project_id}/${var.blue_green_environment}/search-for-errors"
 
   log_group_names = [
@@ -30,10 +30,11 @@ EOF
   lifecycle {
     create_before_destroy = false
   }
+
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_correlation_id" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_for_errors]
   name = "${var.project_id}/${var.blue_green_environment}/search-by-correlation-id"
 
   log_group_names = [
@@ -51,14 +52,13 @@ fields @timestamp, message
 | filter correlation_id == 'REPLACE'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_correlation_id_expanded" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_correlation_id]
   name = "${var.project_id}/${var.blue_green_environment}/search-by-correlation-id-expanded"
 
   log_group_names = [
@@ -76,14 +76,13 @@ fields @timestamp,correlation_id,ods_code,level,message_received,function_name, 
 | filter correlation_id == 'REPLACE'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_odscode" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_correlation_id_expanded]
   name = "${var.project_id}/${var.blue_green_environment}/search-by-odscode"
 
   log_group_names = [
@@ -101,14 +100,13 @@ fields @timestamp, message
 | filter ods_code == 'REPLACE'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_odscode_expanded" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_odscode]
   name = "${var.project_id}/${var.blue_green_environment}/search-by-odscode-expanded"
 
   log_group_names = [
@@ -126,14 +124,13 @@ fields @timestamp,correlation_id,ods_code,level,message_received,function_name, 
 | filter ods_code == 'REPLACE'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_for_invalid_postcode" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_odscode_expanded]
   name = "${var.project_id}/${var.blue_green_environment}/search-for-invalid-postcode"
 
   log_group_names = [
@@ -145,14 +142,13 @@ fields @timestamp,correlation_id,ods_code,level,message_received,function_name, 
 | filter report_key == 'INVALID_POSTCODE'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_for_invalid_opening_times" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_for_invalid_postcode]
   name = "${var.project_id}/${var.blue_green_environment}/search-for-invalid-opening-times"
 
   log_group_names = [
@@ -164,14 +160,13 @@ fields @timestamp,correlation_id,ods_code,level,message_received,function_name, 
 | filter report_key == 'INVALID_OPEN_TIMES'
 | sort @timestamp
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_email_correlation_id" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_for_invalid_opening_times]
   name = "${var.project_id}/${var.blue_green_environment}/search-by-email-correlation-id"
 
   log_group_names = [
@@ -184,14 +179,13 @@ fields correlation_id
 | filter message =="Email Correlation Id"
 | filter email_correlation_id == "ADD_EMAIL_CORRELATION_ID"
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_update_request_success" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_email_correlation_id]
   name = "${var.project_id}/${var.blue_green_environment}/update-request-success"
 
   log_group_names = [
@@ -203,14 +197,13 @@ fields @timestamp, correlation_id
 | filter ServiceUpdateSuccess == 1
 | sort @timestamp desc
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_update_request_failed" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_update_request_success]
   name = "${var.project_id}/${var.blue_green_environment}/update-request-failed"
 
   log_group_names = [
@@ -222,14 +215,13 @@ fields @timestamp, correlation_id, report_key
 | filter report_key == DOS_DB_UPDATE_DLQ_HANDLER_RECEIVED_EVENT
 | sort @timestamp desc
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_by_dos_data_item_updates" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_update_request_failed]
   name = "${var.project_id}/${var.blue_green_environment}/dos-data-item-updates"
 
   log_group_names = [
@@ -243,14 +235,13 @@ fields @timestamp, correlation_id
 | filter field == 'REPLACE'
 | sort @timestamp desc
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
 }
 
 resource "aws_cloudwatch_query_definition" "search_for_report_warnings" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_by_dos_data_item_updates]
   name = "${var.project_id}/${var.blue_green_environment}/search-for-report-warnings"
 
   log_group_names = [
@@ -269,7 +260,6 @@ fields @timestamp, correlation_id, message
 | filter level == 'WARNING'
 | sort @timestamp desc
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
@@ -277,7 +267,7 @@ EOF
 
 
 resource "aws_cloudwatch_query_definition" "search_for_quality_checker_logs_with_odscode" {
-  depends_on = [time_sleep.wait_for_propagation]
+  depends_on = [aws_cloudwatch_query_definition.search_for_report_warnings]
   name = "${var.project_id}/${var.blue_green_environment}/search-for-quality-checker-logs-with-odscode"
 
   log_group_names = [
@@ -289,7 +279,6 @@ fields @timestamp, level, message
 | filter odscode = 'TO_ADD'
 | sort @timestamp asc
 EOF
-
   lifecycle {
     create_before_destroy = false
   }
